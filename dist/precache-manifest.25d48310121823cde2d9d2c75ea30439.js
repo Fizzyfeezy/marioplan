7db656315647b6dcd25d8832d1b3c242e6ef3f7e
oyelami hafeez <oyelami.abdulhafeez@gmail.com>
2019-09-12 13:52:21 +0100

    update

---
+++ dist/precache-manifest.25d48310121823cde2d9d2c75ea30439.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "338f7bdaf1029d8bde5e613019d6af29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40a1ad693913f76fd06a",
+    "url": "/static/css/2.ae9e8b99.chunk.css"
+  },
+  {
+    "revision": "1b40e32df5d88fb623a9",
+    "url": "/static/css/main.5473d1b0.chunk.css"
+  },
+  {
+    "revision": "40a1ad693913f76fd06a",
+    "url": "/static/js/2.1adeb15d.chunk.js"
+  },
+  {
+    "revision": "1b40e32df5d88fb623a9",
+    "url": "/static/js/main.93c8dad5.chunk.js"
+  },
+  {
+    "revision": "f0d41491b4c76a5bff7b",
+    "url": "/static/js/runtime~main.c99bf0a6.js"
+  },
+  {
+    "revision": "43925cfc95adcc64403de064bce517d6",
+    "url": "/static/media/mario.43925cfc.png"
+  }
+]);
